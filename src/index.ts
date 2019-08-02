776bb40f901c608ce7aa2b665349774048f05ee8
Victor Rocha <victor.rocha@zenvia.com>
2019-08-02 19:42:32 -0300

    bubble sort implemented

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('hi there!');
+class Sorter {
+    collection: number[];
+
+    constructor(collection: number[]) {
+        this.collection = collection;
+    }
+
+    bubbleSort(): void {
+        const { length } = this.collection;
+
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHandElement = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHandElement;
+                }
+            }
+        }
+    } 
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.bubbleSort();
+console.log(sorter.collection);
